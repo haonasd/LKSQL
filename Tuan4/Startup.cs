f074e3b2f4699e142fa7b4847b79d87194377146
Hao <https://github.com/haonasd/LKSQL.git>
2019-03-26 14:56:14 +0700

    first commit

---
+++ Tuan4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tuan4.Startup))]
+namespace Tuan4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
